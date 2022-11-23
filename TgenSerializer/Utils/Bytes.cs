9ea80b862aa83eabb3cb44d711dee5b1b90b0bd8
Yoav Haik <yoavhaik@gmail.com>
2022-11-23 12:15:18 +0200

    Changed format

--- TgenSerializer/Utils/Bytes.cs
+++ TgenSerializer/Utils/Bytes.cs
@@ -33,45 +33,6 @@ namespace TgenSerializer
         /// <typeparam name="T">must be a primitive or string type</typeparam>
         public T Get<T>() where T : unmanaged => ByteToPrimitive<T>(this); //Implicit use of the function GetBytes()
 
-#if NETSTANDARD2_1
-        /// <summary>
-        /// Gets a <c>ValueTuple</c> type with primitive item types and converts the Bytes to the given type.
-        /// 
-        /// <example>
-        /// Example:
-        ///      <code>
-        ///      Bytes val = 10;
-        ///      val += 20;
-        ///      
-        ///      (int, int) ret = val.GetTuple&lt;(int, int)&gt;();
-        ///      Console.WriteLine(ret); //(10, 20)
-        ///      </code>
-        /// </example>
-        /// </summary>
-        /// <typeparam name="T">A ValueTuple for example <c>(int, double)</c></typeparam>
-        /// <returns>A tuple from the given array</returns>
-        /// 
-        public T GetTuple<T>() where T : ITuple, new()
-        {
-            T tuple = new T();
-
-            //struct must be boxed since structs are value types.
-            //Otherwise `SetValue` makes a new value then loses it
-            object boxed = tuple; 
-
-            var fields = typeof(T).GetFields(); //REFLECTION
-            int index = 0;
-            for (int i = 0; i < tuple.Length; i++)
-            {
-                Type t = tuple[i].GetType();
-                object val = Bytes.ByteToPrimitive(t, Array, index);
-                fields[i].SetValue(boxed, val);
-                index += Marshal.SizeOf(t);
-            }
-            return (T)boxed;
-        }
-#endif
-
         /// <summary>
         /// Gets a <c>ValueTuple</c> type with primitive item types and converts the Bytes to the given type.
         /// 
@@ -130,5 +91,44 @@ namespace TgenSerializer
         { return Bytes.Concat(a, b); }
         //Used to be 'new Bytes(a).Append(b);'
 
+
+#if NETSTANDARD2_1
+        /// <summary>
+        /// Gets a <c>ValueTuple</c> type with primitive item types and converts the Bytes to the given type.
+        /// 
+        /// <example>
+        /// Example:
+        ///      <code>
+        ///      Bytes val = 10;
+        ///      val += 20;
+        ///      
+        ///      (int, int) ret = val.GetTuple&lt;(int, int)&gt;();
+        ///      Console.WriteLine(ret); //(10, 20)
+        ///      </code>
+        /// </example>
+        /// </summary>
+        /// <typeparam name="T">A ValueTuple for example <c>(int, double)</c></typeparam>
+        /// <returns>A tuple from the given array</returns>
+        /// 
+        public T GetTuple<T>() where T : ITuple, new()
+        {
+            T tuple = new T();
+
+            //struct must be boxed since structs are value types.
+            //Otherwise `SetValue` makes a new value then loses it
+            object boxed = tuple; 
+
+            var fields = typeof(T).GetFields(); //REFLECTION
+            int index = 0;
+            for (int i = 0; i < tuple.Length; i++)
+            {
+                Type t = tuple[i].GetType();
+                object val = Bytes.ByteToPrimitive(t, Array, index);
+                fields[i].SetValue(boxed, val);
+                index += Marshal.SizeOf(t);
+            }
+            return (T)boxed;
+        }
+#endif
     }
 }
