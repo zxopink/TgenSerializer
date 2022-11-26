1195b4350ebbedb47ecd0b0b5868c742b400c61d
Yoav Haik <yoavhaik@gmail.com>
2022-11-27 01:37:03 +0200

    Update Program.cs

--- PlayTest/Program.cs
+++ PlayTest/Program.cs
@@ -2,6 +2,7 @@
 
 Bytes bytes = 10;
 bytes += 20;
+bytes += 50;
 
-var valT = bytes.GetTupleUnsafe<(int, int)>();
+var valT = bytes.GetTupleUnsafe<(int,int,int)>();
 Console.WriteLine(valT); //(10, 20)
